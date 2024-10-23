86d8105b4cbfc0b1c6c05bd7abfdb024e8cd0aaa
D2mmax <max.geraschenko@gmail.com>
2024-10-23 12:03:27 +0100

    Shooting added

---
+++ Assets/RocketJumping.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.PlasticSCM.Editor.UI;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class ShotgunJump : MonoBehaviour
+{
+    public Transform camHolder;      // Reference to the camera holder to determine the direction
+    public Rigidbody playerRb;       // Reference to the player's Rigidbody
+
+    [SerializeField] private KeyCode shootButton = KeyCode.Mouse0;  // Button used to shoot (default: Left Mouse Button)
+    [SerializeField] private float jumpStrength = 10f;              // Strength of the shotgun jump
+    [SerializeField] private GameObject projectile;
+
+    [SerializeField] private float _cooldDown = 3f;
+
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(shootButton) && _canShoot)
+        {
+            StartCoroutine(RocketJump());
+        }
+    }
+
+    private IEnumerator RocketJump()
+    {   
+        _canShoot = false;
+        // Calculate the opposite direction to where the camera is looking
+        Vector3 jumpDirection = -camHolder.forward;
+        
+        // Apply the force to the player Rigidbody in the opposite direction
+        playerRb.AddForce(jumpDirection * jumpStrength, ForceMode.Impulse);
+
+        yield return new WaitForSeconds(_cooldDown);
+
+        _canShoot = true;
+    }
+}
